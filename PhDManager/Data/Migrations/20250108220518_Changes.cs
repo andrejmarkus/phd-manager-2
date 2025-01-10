4f87a2cb650e14eedef1b4949077811559099fc7
gastyCode <andrejmarkus274@gmail.com>
2025-01-10 23:02:50 +0100

    CSS rules and little fixes.

---
+++ PhDManager/Data/Migrations/20250108220518_Changes.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhDManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Matematické princípy informatiky");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Subjects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Matematické peincípy informatiky");
+        }
+    }
+}

e823a173e32abfc1de759dd548534b12d98169e7
gastyCode <andrejmarkus274@gmail.com>
2025-02-14 00:40:59 +0100

    Theses admin page

--- PhDManager/Services/DocumentService.cs
+++ PhDManager/Services/DocumentService.cs
@@ -1,18 +1,20 @@
 ﻿using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.JSInterop;
+using PhDManager.Models;
 using System.Globalization;
 using System.Text;
 
 namespace PhDManager.Services
 {
-    public class DocumentService
+    public class DocumentService(IJSRuntime JSRuntime)
     {
-        public string NormalizeName(string name)
+        private string NormalizeName(string name)
         {
             return new string(name.Normalize(NormalizationForm.FormD).Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark).Select(c => c == ' ' ? '_' : c).ToArray());
         }
 
-        public Stream GenerateDocumentData(string path, Dictionary<string, string> replacements)
+        private Stream GenerateDocumentData(string path, Dictionary<string, string> replacements)
         {
             var documentStream = new MemoryStream();
 
@@ -41,9 +43,38 @@ namespace PhDManager.Services
 
                 document.Clone(documentStream);
             }
-            
+
             documentStream.Seek(0, SeekOrigin.Begin);
             return documentStream;
         }
-    }
+
+        public async Task DownloadThesisDocument(Thesis thesis)
+        {
+            var documentName = NormalizeName(thesis.Title) + ".docx";
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "templates", "thesis_template.docx");
+            var replacements = new Dictionary<string, string>()
+            {
+                {"{Title}", thesis.Title},
+                {"{Supervisor}", thesis.Supervisor.DisplayName},
+                {"{StudyProgram}", thesis.StudyProgram.Name},
+                {"{StudyField}", thesis.StudyProgram.StudyFieldName},
+                {"{DailyStudy}", thesis.DailyStudy ? "☑" : "☐"},
+                {"{ExternalStudy}", thesis.ExternalStudy ? "☑" : "☐"},
+                {"{Subject1}", thesis.SubjectNames[0]},
+                {"{Subject2}", thesis.SubjectNames[1]},
+                {"{Subject3}", thesis.SubjectNames[2]},
+                {"{Description}", thesis.Description},
+                {"{ScientificContribution}", thesis.ScientificContribution},
+                {"{ScientificProgress}", thesis.ScientificProgress},
+                {"{ResearchType}", thesis.ResearchType},
+                {"{ResearchTask}", thesis.ResearchTask},
+                {"{SolutionResults}", thesis.SolutionResults}
+            };
+
+            var document = GenerateDocumentData(path, replacements);
+            using var documentStream = new DotNetStreamReference(document);
+
+            await JSRuntime.InvokeVoidAsync("saveAsFile", documentName, documentStream);
+            }
+        }
 }
